d81986b01ed8e17c85bc8b0173c19028093d1a98
leonardo <leonardo@vetwork.com.br>
2016-07-31 01:10:15 -0300

    exibindo receitas na pagina inicial

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :recipe do
-    name "MyString"
-    kitchen_type "MyString"
-    food_type "MyString"
-    amout_people 1
-    preparation_time "MyString"
-    preparation_level "MyString"
-    ingredients "MyText"
-    steps "MyText"
+    name "Bolo de Cenoura"
+    kitchen_type "Caseiro"
+    food_type "Doce"
+    amount_people 8
+    preparation_time "60 minutos"
+    preparation_level "Fácil"
+    ingredients "Ovo, Leite, Farinha, Fermento, Cenoura, Chocolate"
+    steps "Bata alguns ingredientes, depois junte com outros e coloque no forno durante 40 minutos"
   end
 end
--- spec/features/user_visit_home_spec.rb
+++ spec/features/user_visit_home_spec.rb
@@ -6,4 +6,22 @@ feature 'Visitor visit home' do
 
     expect(page).to have_css('h1', text: 'SocialRecipes')
   end
+
+  scenario 'and view all recipes' do
+    recipe  = create(:recipe)
+    recipe1 = create(:recipe, name: 'Bolo de Laranja')
+    recipe2 = create(:recipe, name: 'Bolo de Fubá')
+
+    recipes = [recipe, recipe1, recipe2]
+
+    visit root_path
+
+    recipes.each do |recipe|
+      expect(page).to have_content(recipe.name)
+      expect(page).to have_content(recipe.kitchen_type)
+      expect(page).to have_content(recipe.food_type)
+      expect(page).to have_content(recipe.preparation_time)
+      expect(page).to have_content(recipe.preparation_level)
+    end
+  end
 end
